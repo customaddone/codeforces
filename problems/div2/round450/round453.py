f39f4b060319f7fcffafc4cac2a9ce16bc366aee
customaddone <custom1485@gmail.com>
2021-04-14 19:52:04 +0900

    round453

--- problems/div2/round450/round453.py
+++ problems/div2/round450/round453.py
@@ -1,33 +1,4 @@
-from collections import defaultdict, deque, Counter
-from heapq import heapify, heappop, heappush
-import math
-from copy import deepcopy
-from itertools import combinations, permutations, product
-from bisect import bisect_left, bisect_right
-
-import sys
-def input():
-    return sys.stdin.readline().rstrip()
-def getN():
-    return int(input())
-def getNM():
-    return map(int, input().split())
-def getList():
-    return list(map(int, input().split()))
-def getArray(intn):
-    return [int(input()) for i in range(intn)]
-
-sys.setrecursionlimit(1000000000)
-mod = 10 ** 9 + 7
-MOD = 998244353
-INF = float('inf')
-eps = 10 ** (-10)
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-#############
-# Main Code #
-#############
+# A. Odds and Ends
 
 """
 # 奇数の長さの奇数で始まり奇数で終わる数列に分割する
